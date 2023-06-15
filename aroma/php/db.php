0ec6e8cddaa19c57c10f80e585611d33871a4dc4
SergeyChizhikov <132288129+SergeyChizhikov@users.noreply.github.com>
2023-06-15 11:26:22 +0300

    Добавил User, реализован addUser

---
+++ aroma/php/db.php
@@ -0,0 +1,2 @@
+<?php
+$mysqli = mysqli_connect("localhost", "oefnbvtr_0155", "123456", "oefnbvtr_0155");
--- aroma/router.php
+++ aroma/router.php
@@ -1,6 +1,8 @@
 <?php
-
+session_start();
 $url = explode('/', $_SERVER['REQUEST_URI']);
+require_once("php/db.php");
+require_once("php/classes/User.php");
 
 
 if ($url[1] == "auth") {
@@ -11,6 +13,10 @@ if ($url[1] == "auth") {
   $content = file_get_contents("pages/blog.html");
 } else if ($url[1] == "users") {
   require_once("pages/users/index.html");
+} else if ($url[1] == "addUser") {
+  echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "authUser") {
+  echo User::authUser($_POST["email"], $_POST["pass"]);
 } else {
   $content = file_get_contents("pages/index.php");
 }
