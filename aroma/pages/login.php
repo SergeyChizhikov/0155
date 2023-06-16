66bba4739159d10af2ae286a917e0952f2ec78d6
SergeyChizhikov <132288129+SergeyChizhikov@users.noreply.github.com>
2023-06-16 12:14:59 +0300

    Новый функционал User

--- aroma/pages/login.php
+++ aroma/pages/login.php
@@ -3,11 +3,11 @@
 		<div class="container h-100">
 			<div class="blog-banner">
 				<div class="text-center">
-					<h1>Login / Register</h1>
+					<h1>Авторизация / Регистрация</h1>
 					<nav aria-label="breadcrumb" class="banner-breadcrumb">
 						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="#">Home</a></li>
-							<li class="breadcrumb-item active" aria-current="page">Login/Register</li>
+							<li class="breadcrumb-item"><a href="/">Главная</a></li>
+							<li class="breadcrumb-item active" aria-current="page">Вход/Регистрация</li>
 						</ol>
 					</nav>
 				</div>
@@ -23,31 +23,25 @@
 				<div class="col-lg-6">
 					<div class="login_box_img">
 						<div class="hover">
-							<h4>New to our website?</h4>
-							<p>There are advances being made in science and technology everyday, and a good example of this is the</p>
-							<a class="button button-account" href="register.html">Create an Account</a>
+							<h4>Впервые на нашем сайте?</h4>
+							<p>Зарегистрируйтесь, получите доступ к личному кабинету и морю подарков и скидок</p>
+							<a class="button button-account" href="/register">Идти регистрироваться</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-6">
 					<div class="login_form_inner">
-						<h3>Log in to enter</h3>
-						<form class="row login_form" action="#/" id="contactForm">
+						<h3>Вход в личный кабинет</h3>
+						<form class="row login_form" onsubmit="sendForm(this); return false;">
 							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+								<input type="email" class="form-control" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" required>
 							</div>
 							<div class="col-md-12 form-group">
-								<input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+								<input type="password" class="form-control" name="pass" placeholder="Пароль" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Пароль'" required>
 							</div>
+							<p id="info" style="color: #c5322d; padding-left: 15px;"></p>
 							<div class="col-md-12 form-group">
-								<div class="creat_account">
-									<input type="checkbox" id="f-option2" name="selector">
-									<label for="f-option2">Keep me logged in</label>
-								</div>
-							</div>
-							<div class="col-md-12 form-group">
-								<button type="submit" value="submit" class="button button-login w-100">Log In</button>
-								<a href="#">Forgot Password?</a>
+								<button type="submit" class="button button-login w-100">Войти</button>
 							</div>
 						</form>
 					</div>
@@ -56,3 +50,39 @@
 		</div>
 	</section>
 	<!--================End Login Box Area =================-->
+	<!-- Модальное окно -->
+	<div class="modal fade" id="myModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Вы успешно авторизованы!</h5>
+				</div>
+				<div class="modal-body">
+					И будете перенаправлены в личный кабинет через 3 секунды.
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		async function sendForm(form) {
+			let formData = new FormData(form);
+			let response = await fetch("authUser", {
+				method: "POST",
+				body: formData,
+			})
+
+			let res = await response.json();
+			if (res.result == "ok") {
+				console.log("Ура!");
+				$('#myModal').modal('show');
+				setTimeout(() => {
+					location.href = "/users/profile";
+				}, 3000)
+			} else if (res.result == "rejected") {
+				info.innerText = "Неправильный логин или пароль";
+			} else {
+				alert("Неизвестная ошибка");
+			}
+		}
+	</script>
--- aroma/php/classes/User.php
+++ aroma/php/classes/User.php
@@ -14,13 +14,26 @@ class User
     $this->lastname = $lastname;
     $this->email = $email;
   }
-  function getId() {return $this->id;}
-  function getName() {return $this->name;}
-  function getLastname() {return $this->lastname;}
-  function getEmail() {return $this->email;}
+  function getId()
+  {
+    return $this->id;
+  }
+  function getName()
+  {
+    return $this->name;
+  }
+  function getLastname()
+  {
+    return $this->lastname;
+  }
+  function getEmail()
+  {
+    return $this->email;
+  }
 
   //Статический метод добавления пользователя
-  static function addUser($name, $lastname, $email, $pass) {
+  static function addUser($name, $lastname, $email, $pass)
+  {
     global $mysqli;
     $email = mb_strtolower(trim($email));
     $pass = trim($pass);
@@ -29,17 +42,50 @@ class User
     $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
 
     if ($result->num_rows != 0) {
-      return json_encode(["result"=>"exist"]);
+      return json_encode(["result" => "exist"]);
     } else {
       $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name', '$lastname', '$email', '$pass')");
-      return json_encode(["result"=>"success"]);
+      return json_encode(["result" => "success"]);
     }
-
   }
 
   //Статический метод авторизации пользователя
-  static function authUser($email, $pass) {
+  static function authUser($email, $pass)
+  {
     global $mysqli;
-    return "Пользователь авторизован";
+
+    $email = trim(mb_strtolower($email));
+    $pass = trim($pass);
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
+    $result = $result->fetch_assoc();
+
+
+    if (password_verify($pass, $result["pass"])) {
+      $_SESSION["id"] = $result["id"];
+      return json_encode(["result" => "ok"]);
+    } else {
+      return json_encode(["result" => "rejected"]);
+    }
+  }
+  //Статический метод получения одного пользователя
+  static function getUser($userId)
+  {
+    global $mysqli;
+    $result = $mysqli->query("SELECT `id`, `name`, `lastname`, `email` FROM `users` WHERE `id` = '$userId'");
+    $result = $result->fetch_assoc();
+    return json_encode($result);
+  }
+  //Статический метод получения всех пользователей
+  static function getUsers() {
+    global $mysqli;
+    $users = array();
+
+    $result = $mysqli->query("SELECT `id`, `name`, `lastname`, `email` FROM `users` WHERE 1");
+
+    while($row = $result->fetch_assoc()) {
+      $users[] = $row;
+    }
+
+    return json_encode($users);
   }
 }
